17e8ecb281f2e8b74e103410ba30072dd47a5e9b
liaohengfeng <18982620158@163.com>
2024-12-20 21:27:00 +0800

    upd by deploy.sh

---
+++ js/inpack-css.js
@@ -0,0 +1,2 @@
+if (window.location.pathname == "/music/") document.write('<link rel="stylesheet" href="/js/music.css">');
+if (window.location.pathname != "/") document.write('<link rel="stylesheet" href="/js/hover.css">');
